10dce8f2b8e5554834272382223c6334bdb96bf2
pw4ch7re3 <pwchre@gmail.com>
2026-04-29 11:21:30 +0900

    Test B tree impl

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,12 @@
 #include "include/loader.h"
+#include "include/b_tree.h"
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <cerrno>
 #include <filesystem>
+
+static void test_btree (Record *);
 
 int
 main ()
@@ -32,7 +35,37 @@ main ()
       exit (EXIT_FAILURE);
     }
 
+#if 0
   show_table (table);
+#endif
+
+#if 1
+  test_btree (table);
+#endif
 
   free_table (table);
+}
+
+static void
+test_btree (Record *table)
+{
+  BTree *btree = new BTree (3);
+  size_t n_rows = 10;
+  int k = 0;
+  Record *record;
+
+  for (size_t i = 0; i < n_rows; i++)
+    {
+      btree->insert_item (table[i].student_id, &table[i]);
+      record = btree->search_item (table[i].student_id);
+      if (record) printf ("Student %d inserted\n", table[i].student_id);
+
+      if (k == 0) k = table[i].student_id;
+    }
+
+  btree->delete_item (k);
+  record = btree->search_item (k);
+  if (!record) printf ("Student %d deleted\n", k);
+
+  delete btree;
 }
