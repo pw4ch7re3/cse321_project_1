a66539dc86cabd694ccaaf732f670cfc59d68550
pw4ch7re3 <pwchre@gmail.com>
2026-04-30 22:39:06 +0900

    Write B* tree header

--- src/include/bs_tree.h
+++ src/include/bs_tree.h
@@ -6,4 +6,56 @@
 #ifndef BS_TREE_H
 #define BS_TREE_H
 
+#include "node.h"
+
+class BSTree
+{
+  size_t d;
+  Node *root;
+
+  /* Helper functions */
+
+  void
+  delete_tree (Node *x)
+  {
+    if (!x) return;
+
+    if (!x->leaf)
+      for (size_t i = 0; i <= x->n; i++)
+        delete_tree (x->p[i]);
+
+    delete x;
+  }
+
+  void create_tree () { root = allocate_node (d); }
+
+  Node *search_node (Node *x, int k, size_t &i);
+
+  void split_child (Node *x, size_t i);
+  void insert_nonfull (Node *x, int k, Record *v);
+
+  void merge_siblings (Node *x, size_t i);
+  void delete_node (Node *x, int k);
+
+public:
+  BSTree (size_t d) : d (d), root (allocate_node (d)) {}
+  ~BSTree () { delete_tree (root); }
+
+  /* Basic operations */
+
+  Node *search_tree (int k, size_t &i);
+  void insert_item (int k, Record *v);
+  void delete_item (int k);
+
+  /* Wrapper functions */
+
+  Record *
+  search_item (int k)
+  {
+    size_t i;
+    Node *x = search_tree (k, i);
+    return x ? x->v[i] : nullptr;
+  }
+};
+
 #endif // BS_TREE_H
