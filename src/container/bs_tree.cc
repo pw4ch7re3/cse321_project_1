3593fe427055234200688fa6672d23ebf0fe4d6b
pw4ch7re3 <pwchre@gmail.com>
2026-05-01 17:24:19 +0900

    Impl B* tree

--- src/container/bs_tree.cc
+++ src/container/bs_tree.cc
@@ -285,3 +285,254 @@ BSTree::insert_item (int k, Record *v)
 }
 
 /* Deletion */
+
+void
+BSTree::merge_siblings (Node *x, size_t i)
+{
+  size_t n = (4 * d - 2) / 3;
+  Node *y = x->p[i];
+  size_t j;
+
+  if (y->n >= n) { /* do nothing */ }
+  else if (i > 0 && x->p[i - 1]->n > n)
+    {
+      Node *z = x->p[i - 1];
+      
+      for (j = y->n; j > 0; j--)
+        {
+          y->k[j] = y->k[j - 1];
+          y->v[j] = y->v[j - 1];
+        }
+
+      if (!y->leaf)
+        for (j = y->n + 1; j > 0; j--)
+          y->p[j] = y->p[j - 1];
+
+      y->k[0] = x->k[i - 1];
+      y->v[0] = x->v[i - 1];
+
+      if (!y->leaf) y->p[0] = z->p[z->n];
+
+      y->n++;
+
+      x->k[i - 1] = z->k[z->n - 1];
+      x->v[i - 1] = z->v[z->n - 1];
+
+      z->n--;
+    }
+  else if (i < x->n && x->p[i + 1]->n > n)
+    {
+      Node *z = x->p[i + 1];
+      
+      y->k[y->n] = x->k[i];
+      y->v[y->n] = x->v[i];
+
+      if (!y->leaf) y->p[y->n + 1] = z->p[0];
+      
+      y->n++;
+
+      x->k[i] = z->k[0];
+      x->v[i] = z->v[0];
+
+      for (j = 0; j < z->n - 1; j++)
+        {
+          z->k[j] = z->k[j + 1];
+          z->v[j] = z->v[j + 1];
+        }
+      
+      if (!z->leaf)
+        for (j = 0; j < z->n; j++)
+          z->p[j] = z->p[j + 1];
+          
+      z->n--;
+    }
+  else if (x->n == 1)
+    {
+      Node *z = x->p[0];
+      Node *w = x->p[1];
+      
+      z->k[z->n] = x->k[0];
+      z->v[z->n] = x->v[0];
+      
+      for (j = 0; j < w->n; j++)
+        {
+          z->k[z->n + 1 + j] = w->k[j];
+          z->v[z->n + 1 + j] = w->v[j];
+        }
+
+      if (!z->leaf)
+        for (j = 0; j <= w->n; j++)
+          z->p[z->n + 1 + j] = w->p[j];
+          
+      z->n += w->n + 1;
+      x->n = 0;
+
+      delete w;
+    }
+  else
+    {
+      size_t i_ = i == x->n ? i - 2 : (i == 0 ? 0 : i - 1);
+      
+      Node *z = x->p[i_];
+      Node *w = x->p[i_ + 1];
+      Node *u = x->p[i_ + 2];
+
+      size_t n_children = z->n + w->n + u->n + 2;
+      
+      Node **p = new Node *[n_children + 1];
+      int *k = new int[n_children];
+      Record **v = new Record *[n_children];
+
+      size_t c = 0; // cursor
+      
+      for (j = 0; j < z->n; j++)
+        {
+          k[c] = z->k[j];
+          v[c] = z->v[j];
+
+          p[c] = z->p[j];
+
+          c++;
+        }
+
+      p[c] = z->p[z->n];
+      
+      k[c] = x->k[i_];
+      v[c] = x->v[i_];
+      
+      c++;
+      
+      for (j = 0; j < w->n; j++)
+        {
+          k[c] = w->k[j];
+          v[c] = w->v[j];
+
+          p[c] = w->p[j];
+
+          c++;
+        }
+
+      p[c] = w->p[w->n];
+      
+      k[c] = x->k[i_ + 1];
+      v[c] = x->v[i_ + 1];
+      
+      c++;
+      
+      for (j = 0; j < u->n; j++)
+        {
+          k[c] = u->k[j];
+          v[c] = u->v[j];
+
+          p[c] = u->p[j];
+
+          c++;
+        }
+
+      p[c] = u->p[u->n];
+
+      size_t n_z = (n_children - 1) / 2;
+      size_t n_w = n_children - 1 - n_z;
+
+      z->n = n_z;
+      w->n = n_w;
+
+      c = 0;
+
+      for (j = 0; j < z->n; j++)
+        {
+          z->k[j] = k[c];
+          z->v[j] = v[c];
+
+          z->p[j] = p[c];
+
+          c++;
+        }
+      
+      z->p[z->n] = p[c];
+
+      x->k[i_] = k[c];
+      x->v[i_] = v[c];
+      
+      c++;
+
+      for (j = 0; j < w->n; j++)
+        {
+          w->k[j] = k[c];
+          w->v[j] = v[c];
+          
+          w->p[j] = p[c];
+
+          c++;
+        }
+
+      w->p[w->n] = p[c];
+
+      for (j = i_ + 1; j < x->n - 1; j++)
+        {
+          x->k[j] = x->k[j + 1];
+          x->v[j] = x->v[j + 1];
+
+          x->p[j + 1] = x->p[j + 2];
+        }
+
+      x->n--;
+
+      delete u;
+
+      delete[] p;
+      delete[] k;
+      delete[] v;
+    }
+}
+
+void
+BSTree::delete_node (Node *x, int k)
+{
+  size_t i = 0, j;
+  while (i < x->n && k > x->k[i]) i++;
+
+  if (x->leaf && i < x->n && k == x->k[i])
+    {
+      for (j = i; j < x->n - 1; j++)
+        {
+          x->k[j] = x->k[j + 1];
+          x->v[j] = x->v[j + 1];
+        }
+
+      x->n--;
+    }
+  else if (!x->leaf && i < x->n && k == x->k[i])
+    {
+      Node *w = x->p[i];
+      while (!w->leaf) w = w->p[w->n];
+      
+      x->k[i] = w->k[w->n - 1];
+      x->v[i] = w->v[w->n - 1];
+      
+      delete_node (x->p[i], x->k[i]);
+      merge_siblings (x, i);
+    }
+  else
+    {
+      delete_node (x->p[i], k);
+      merge_siblings (x, i);
+    }
+}
+
+void
+BSTree::delete_item (int k)
+{
+  if (!root) return;
+
+  delete_node (root, k);
+
+  if (!root->leaf && root->n == 0)
+    {
+      Node *r = root;
+
+      root = root->p[0];
+      
+      delete r;
+    }
+}
--- src/container/list.cc
+++ src/container/list.cc
@@ -5,11 +5,11 @@ allocate_list (List *next, int d, bool leaf)
 {
   List *x = new List;
   x->leaf = leaf;
-  x->p = new List *[2 * d + 1];
-  x->k = new int[2 * d];
+  x->p = new List *[2 * d + 1] ();
+  x->k = new int[2 * d] ();
   
   if (leaf)
-    x->v = new Record *[2 * d];
+    x->v = new Record *[2 * d] ();
   
   x->next = next;
   
